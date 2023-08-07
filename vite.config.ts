df3cf6522177939760233e74914f55ebb2150566
gurievvlad <guriev.vladislav@icloud.com>
2023-08-07 15:53:49 +0500

    Исправленный конфиг билда

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
           const info = assetInfo.name.split('.')
           let extType = info[info.length - 1]
           if (
-            /\.(mp4|webm|ogg|mp3|wav|wma|flac|aac|3gp|avi|flv|mkv|mov|rmvb|ts|vob|webm|wmv)(\?.*)?$/i.test(assetInfo.name)
+            /\.(mp4|ogg|mp3|wav|wma|flac|aac|3gp|avi|flv|mkv|mov|rmvb|ts|vob|webm|wmv)(\?.*)?$/i.test(assetInfo.name)
           ) {
             extType = 'media'
           } else if (/\.(png|jpe?g|gif|svg|bmp|tiff|webp)(\?.*)?$/i.test(assetInfo.name)) {
